20ca3b4a5052f0b8836faffc9a4d477bf891f563
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-04-11 14:06:55 -0400

    commit tree 5489d6a3734083a84520b54656ff4f917df79d18

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+
+
+function writeCards(name, event) {
+    const gifts = [];
+    for (let i = 0; i < name.length; i++) {
+        gifts.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return gifts;
+}
+
+
+
+
+
+function countDown(i) {
+    while (i >=0) {
+        console.log(i);
+        i--;
+    }
+    }
